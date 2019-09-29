8c0126b75fd935968a6f5a1cb59fbfc77b69376f
Reni21 <r.phoenixart@gmail.com>
2019-09-30 02:05:25 +0300

    Add test for exception case for ElectricalApplianceService

--- src/test/java/service/ElectricalApplianceServiceTest.java
+++ src/test/java/service/ElectricalApplianceServiceTest.java
@@ -1,12 +1,10 @@
 package service;
 
-import entity.ApplianceBrand;
-import entity.ApplianceColor;
-import entity.ApplianceName;
 import entity.ElectricalAppliance;
 import exception.BusinessException;
 import org.junit.Before;
 import org.junit.Test;
+import util.DataProvider;
 
 import static org.junit.Assert.*;
 
@@ -17,8 +15,7 @@ public class ElectricalApplianceServiceTest {
     @Before
     public void setUp() throws Exception {
         this.instance = new ElectricalApplianceService();
-        this.appliance = new ElectricalAppliance(200, false, 0.45,
-                ApplianceColor.YELLOW, ApplianceName.HAIRDRYER, ApplianceBrand.PHILIPS);
+        this.appliance = DataProvider.createLamp();
     }
 
     @Test
@@ -28,24 +25,56 @@ public class ElectricalApplianceServiceTest {
         assertTrue(res);
     }
 
+    @Test(expected = BusinessException.class)
+    public void shouldThrowBusinessExceptionIfElectricalApplianceIsAlreadyConnectToSocket() throws BusinessException {
+        appliance.setConnectToSocket(true);
+        instance.connectToSocket(appliance);
+    }
+
     @Test
     public void shouldSetIsConnectToSocketInFalse() throws BusinessException {
+        appliance.setConnectToSocket(true);
+
         instance.disconnectFromSocket(appliance);
         boolean res = appliance.isConnectToSocket();
         assertFalse(res);
     }
 
+    @Test(expected = BusinessException.class)
+    public void shouldThrowBusinessExceptionIfElectricalApplianceIsAlreadyDisconnectFromSocket() throws BusinessException {
+        appliance.setConnectToSocket(false);
+        instance.disconnectFromSocket(appliance);
+    }
+
     @Test
     public void shouldSetIsTurnOnInTrue() throws BusinessException {
+        appliance.setConnectToSocket(true);
+
         instance.turnOn(appliance);
         boolean res = appliance.isTurnOn();
         assertTrue(res);
     }
 
+    @Test(expected = BusinessException.class)
+    public void shouldThrowBusinessExceptionIfElectricalApplianceIsAlreadyTurnedOn() throws BusinessException {
+        appliance.setConnectToSocket(true);
+        appliance.setTurnOn(true);
+        instance.turnOn(appliance);
+    }
+
     @Test
     public void shouldSetIsTurnOnInFalse() throws BusinessException {
+        appliance.setTurnOn(true);
+
         instance.turnOff(appliance);
         boolean res = appliance.isTurnOn();
         assertFalse(res);
     }
+
+    @Test(expected = BusinessException.class)
+    public void shouldThrowBusinessExceptionIfElectricalApplianceIsAlreadyTurnedOff() throws BusinessException {
+        appliance.setTurnOn(false);
+        instance.turnOff(appliance);
+    }
+
 }
